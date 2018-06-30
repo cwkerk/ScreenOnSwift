1a3367bfb3503b884219d9bf4017d20efc48467f
Chin Kerk <kerkchinwee@hotmail.co.uk>
2018-06-30 20:40:47 +0800

    Update project

---
+++ ScreenOn/Controller/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  ScreenOn
+//
+//  Created by Chin Wee Kerk on 30/06/2018.
+//  Copyright © 2018 Chin Wee Kerk. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
