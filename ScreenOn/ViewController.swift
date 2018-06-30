2b0688a7d45c0e613710a4bc3d78b0b060b5137e
Chin Kerk <kerkchinwee@hotmail.co.uk>
2018-06-30 08:21:37 +0800

    Initial Commit

---
+++ ScreenOn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenOn
+//
+//  Created by Chin Wee Kerk on 30/06/2018.
+//  Copyright © 2018 Chin Wee Kerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
