d55ea4594dbafcebb38d6e3d5288dcab4ee1a360
Chin Kerk <kerkchinwee@hotmail.co.uk>
2018-07-01 12:27:40 +0800

    Update project

--- ScreenOn/Model/Database/CoreDataManager.swift
+++ ScreenOn/Model/Database/CoreDataManager.swift
@@ -11,4 +11,39 @@ import CoreData
 
 class CoreDataManager {
     
+    static let shared = CoreDataManager()
+    
+    private let appName = "ScreenOn"
+    
+    private lazy var context: NSManagedObjectContext = {
+        if #available(iOS 10.0, *) {
+            let container = NSPersistentContainer(name: self.appName)
+            container.loadPersistentStores(completionHandler: { (desc, error) in
+                if let err = error {
+                    print("Failed to load persistent store to core data context due to: \(err)")
+                }
+            })
+            return container.viewContext
+        } else {
+            let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            do {
+                context.persistentStoreCoordinator = try NSPersistentStoreCoordinator.coordinator(named: self.appName)
+            } catch let error {
+                fatalError("Failed to add persistent store coordinator to core data context due to: \(error)")
+            }
+            return context
+        }
+        
+    }()
+    
+    func saveContext () {
+        if self.context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                fatalError("Failed to save data to core data context due to: \(error)")
+            }
+        }
+    }
+    
 }
